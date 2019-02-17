53e8ba0c055b9176634b618e5342cfcddeb70de9
Andrew Palmer <apalmer0@users.noreply.github.com>
2019-02-17 07:58:54 -0500

    Update gems and use new shoulda_matcher syntax (#33)

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Image, type: :model do
   describe "validations" do
     it { should validate_presence_of(:url) }
-    it { should define_enum_for(:image_source).with({google: 1}) }
-    it { should define_enum_for(:status).with([:active, :inactive]) }
+    it { should define_enum_for(:image_source).with_values({google: 1}) }
+    it { should define_enum_for(:status).with_values([:active, :inactive]) }
   end
 
   describe "set_defaults" do
