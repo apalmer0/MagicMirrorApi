902b6397cd0e66b7db700ca9f62148051d0b7fdb
Andrew Palmer <andrew.palmer.10@gmail.com>
2020-12-01 09:49:09 -0500

    Sort todo items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -15,7 +15,7 @@ module Api
     end
 
     def items_collection
-      @items = Item.all
+      @items = Item.all.order(due: :asc)
       @items = @items.due_today if due_today?
       @items = @items.for_user(params["user_id"]) if for_user?
       @items = @items.incomplete if incomplete?
